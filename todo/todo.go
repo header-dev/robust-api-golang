a6ccce9ab3aedc0d36a0178d0b752cddd3ec49a0
Kritawit.Ya <headerdev@gmail.com>
2024-12-07 14:18:32 +0700

    Initial Project

--- todo/todo.go
+++ todo/todo.go
@@ -1,6 +1,7 @@
 package todo
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,7 @@ import (
 )
 
 type Todo struct {
+	ID    uint   `json:"id" gorm:"primaryKey"`
 	Title string `json:"text"`
 	gorm.Model
 }
@@ -17,16 +19,34 @@ func (Todo) Tablename() string {
 }
 
 type TodoHandler struct {
-	gorm *gorm.DB
+	db *gorm.DB
+}
+
+func NewTodoHandler(db *gorm.DB) *TodoHandler {
+	return &TodoHandler{db: db}
 }
 
 func (t *TodoHandler) NewTask(c *gin.Context) {
 	var todo Todo
+
+	log.Println("New Task", &todo.Title)
 	if err := c.ShouldBindJSON(&todo); err != nil {
+		log.Println("Error binding JSON:", err)
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
+
+	r := t.db.Create(&todo)
+	if err := r.Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"ID": todo.Model.ID,
+	})
 
 }
